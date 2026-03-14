148a90e6b76fcd9bb5e25badf034056e9da89bf5
lafleurblanche <aquamint@squall.dti.ne.jp>
2026-03-14 23:16:33 +0900

    20260314: mod

--- src/main/kotlin/net/konohana/sakuya/konopass/qr/infrastructure/entity/dto/TReaderSettingData.kt
+++ src/main/kotlin/net/konohana/sakuya/konopass/qr/infrastructure/entity/dto/TReaderSettingData.kt
@@ -4,7 +4,7 @@ package net.konohana.sakuya.konopass.qr.infrastructure.entity.dto
  * データクラス: 機器設定データ
  */
 data class TReaderSettingData(
-    val id: Int,
+    val id: Int?,
     val readerId: String?, // Nullableにするか、DBのNOT NULL制約を確認
     val mode: String?,      // Nullableにするか、DBのNOT NULL制約を確認
     val fromStaCode: String?, // Nullableにするか、DBのNOT NULL制約を確認
